4f6eadabe46a7bdb3edd8bc7a81b780ba8847876
hamimhamim21 <hamim.hamim21@gmail.com>
2020-12-19 16:46:11 +1100

    Hello

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ new = []
 for i in results:
     a = {"id":i[0],"state":i[1],"abbr":i[2],"poverty":i[3],"povertyMoe":i[4],"age":i[5],"ageMoe":i[6],"income":i[7],"incomeMoe":i[8],"healthcare":i[9],"healthcareLow":i[10],"healthcareHigh":i[11],"obesity":i[12],"obesityLow":i[13],"obesityHigh":i[14],"smokes":i[15],"smokesLow":i[16],"smokesHigh":i[17]}
     new.append(a)
-
 #################################################
 # Flask Setup
 #################################################
